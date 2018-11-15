53759f4a4eee150c58a3f6a2846b968dcb0079ae
Vinod Kumar <vinodrocky504@gmail.com>
2018-11-15 08:55:35 +0530

    init

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
+# Neighborhood-Map
+
+## Table of Contents
+
++ Description of Project
++ Instructions
++ Dependencies
++ Resources
++ Contributing
+
+### Description of PROJECT
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+### Instructions
+Clone the repo https://github.com/vinodkumarrocky/Neighborhood-Map
+Add your Google maps API key in the App.js file and also have your Four Square API client ID and client secret ID which should also be entered in the App.js file.
 
 In the project directory, you can run:
 
-### `npm start`
+#### `npm start`
 
 Runs the app in the development mode.<br>
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
@@ -12,33 +25,20 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br>
 You will also see any lint errors in the console.
 
-### `npm test`
+### Dependencies
+This project uses Google Maps API and third party ApI called Four Square API. It also uses Axios to install Axios use the JavaScript
+#### 'npm install axios'
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Resources
 
-### `npm run build`
+Webinars: Neighborhood Map (P7) Sept-30 six walk-thrus created by @Forrest (FEND) - YouTube playlist
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
+Udacity | Neighborhood Map [1] - Intro and Creating our React App by Yahya Elharony
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+## Contributing
+
+Welcome
--- src/App.js
+++ src/App.js
@@ -1,28 +1,196 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  state = {
+    venues:[],
+    markers:[],
+    hamburgerActive: false
   }
+  componentDidMount(){
+    this.getVenues()
+  }
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDBuTMyBD4GlwIC2panKoa31VzgNLFkIU4&callback=initMap")
+    window.initMap = this.initMap;
+  }
+
+  getVenues = () => {
+    const api = 'https://api.foursquare.com/v2/venues/explore?';
+    const param = {
+      client_id: 'LEMQI0VL0ADKRLN23NJV1LJ3JW0JYERQJBOKJ10SLOZAJH42',
+      client_secret:'CM5ERC0CFCTIQPHZLVH4KPRVMKFWZUQNYZ5NT0EWLGC25PE5',
+      query:'burger',
+      limit:20,
+      near:'India',
+      v:'20181311'
+    }
+
+    axios.get(api + new URLSearchParams(param))
+    .then(response => {
+      this.setState({
+        venues :response.data.response.groups[0].items
+      },this.renderMap())
+    })
+    .catch(error => {
+      console.log(error);
+    })
 }
 
+initMap = () => {
+  let map = new window.google.maps.Map(document.getElementById('map'),{
+    center :{ lat:20.5937, lng:78.9629},
+    zoom:7
+  });
+
+  this.map = map;
+  let infowindow = new window.google.maps.InfoWindow();
+  const allMarkers = [];
+
+  this.setState({
+    map: map,
+    infowindow:infowindow
+  });
+
+  this.state.venues.forEach(venueinfo => {
+    let contentString =`<div id="info" tabindex="1">
+        <div><h2>${venueinfo.venue.name}</h2></div>
+        <div><h3>${venueinfo.venue.location.address}</h3></div>
+        </div>`;
+    let marker = new window.google.maps.Marker({
+      position:{
+      lat:venueinfo.venue.location.lat,
+      lng:venueinfo.venue.location.lng
+    },
+    map: map,
+    city: venueinfo.venue.location.city,
+    address: venueinfo.venue.location.address,
+    venueinfo: venueinfo,
+    id: venueinfo.venue.id,
+    name: venueinfo.venue.name,
+    draggable: true,
+    animation: window.google.maps.Animation.DROP,
+    title: venueinfo.venue.name
+    });
+
+    marker.addListener("click",function() {
+      // body...
+      let pos = map.getZoom();
+      infowindow.setContent(contentString);
+      infowindow.open(map, marker);
+      map.setZoom(13);
+      map.setCenter(marker.getPosition());
+       window.setTimeout(function() {map.setZoom(pos);},3000);
+      });
+
+    allMarkers.push(marker);
+  });
+
+  this.setState({
+    markers: allMarkers
+  });
+
+  this.setState({
+    filtermyvenue: this.state.venues
+  });
+};
+  constructor(props){
+    super(props);
+    this.state = {
+      sidebar:false,
+      query:""
+    };
+  }
+
+listItemClick = (venues) => {
+  //checking for matchiing id
+  let marker = this.state.markers.filter(m => m.id === venues.id)[0];
+
+//adding city and address
+  this.state.infowindow.setContent(`${marker.name +
+    ", " +
+  marker.city + ", " + marker.address}`);
+
+  //set the map positon to marker positon
+  this.map.setCenter(marker.position);
+  //open infowindow
+  this.state.infowindow.open(this.state.map, marker);
+  
+}
+
+filtermyvenue(query) {
+  let f = this.state.venues.filter(myvenue => myvenue.venue.name.toLowerCase().includes(query.toLowerCase()))
+  
+  //show the infowindow 
+  this.state.markers.forEach(marker => {
+      marker.name.toLowerCase().includes(query.toLowerCase()) === true ?
+    marker.setVisible(true) :
+    marker.setVisible(false);
+  });
+  if (f.length === 0) {
+    //this will close the infowindow  
+    this.state.infowindow.close();   
+  }
+  this.setState({filtermyvenue: f, query}); 
+}
+
+// Toggle sidebar
+  onSidebarClick = () => {
+    const sidebar = document.querySelector('#sidebar');
+    if (this.state.hamburgerActive) {
+      sidebar.style.transform = 'translateX(-250px)'
+      this.setState({ hamburgerActive: false });
+    }
+    else {
+      sidebar.style.transform = 'translateX(0px)'
+      this.setState({sidebar: true});
+      this.setState({ hamburgerActive: true });
+    }
+  }
+
+  render() {
+    return (  
+      <main>
+      <div className="header" aria-label="name">
+        <button aria-label='Hamburger Menu' tabIndex='0' role="menu" className="hamburger-container" onClick={this.onSidebarClick}> 
+          <div className="hamburger-bar"></div>
+          <div className="hamburger-bar"></div>
+          <div className="hamburger-bar"></div>
+        </button>
+        <div className="title"><strong>Restaurants</strong></div></div>
+          
+        <div role="application" aria-label="map" id='map'></div>
+        
+          <div aria-label="sidebar" role="search menu" id='sidebar' >
+            <input type="text" autoFocus="autofocus" tabIndex="0" className="SearchVenues" placeholder="Search venues" value={this.state.query} onChange={(e)=>{this.filtermyvenue(e.target.value)}}/>
+            <br/>
+            <br/>
+            {
+              this.state.filtermyvenue && this.state.filtermyvenue.length > 0 && this.state.filtermyvenue.map((myvenue, index) => (
+                  <div tabIndex="-1" key={index} className="venue-item">
+                      {/* <h4>{myvenue.venue.name}</h4> */}
+                      <ul><li aria-label="search" onClick={()=>{this.listItemClick(myvenue.venue)}}>{myvenue.venue.name}</li>
+                  </ul>
+                  </div>
+              ))
+            }
+        </div>
+      </main>      
+    )
+  }
+}
+function loadScript(source) {
+  try {
+    var index = window.document.getElementsByTagName('script')[0]
+    var script = window.document.createElement('script')
+    script.src = source
+    script.async = true
+    script.defer = true
+    index.parentNode.insertBefore(script, index)
+   } catch (error) {
+    console.log(error);
+   alert("Google Maps API not loading");
+   }
+  }
 export default App;
